4b65b5a9431ce246c6d585c3519f8745568388fc
diego-faria-br <diego-faria-br@users.noreply.github.com>
2020-11-01 15:38:31 +0100

    Diego

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Alethea
 Python Project - Autumn, 2020
+This file has a description of the modules already implemented as well as
+instructions to run the web application
+
+alethea_app module:
+- This is the module that aggregates all other modules to produce a web 
+application, using the Flask library. This module uses the functions that get 
+the information from the twitter API "gather_twitts" and also the fuction that
+produces the graphics
+ 
+ Render html template that in the next days will be improved
+ Produces a real time web application, that is automatically updated.
+ 
--- app_flask/alethea_app.py
+++ app_flask/alethea_app.py
@@ -1,20 +1,24 @@
 """Flask Application for Paws Rescue Center."""
 from flask import Flask, render_template
+
 app = Flask(__name__)
+
 from gather_twits import gather_twits
-from draw_graph import draw_graph
+
+#from draw_graph import draw_graph
 
 
 @app.route("/")
 def homepage():
     """View function for Home Page."""
     twits = gather_twits(n=1, df=False)
-    draw_graph(20)
+ #   draw_graph(20)
     return render_template("home.html", twits = twits)
-
+    
 
 
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=3000)
+    app.run()
+    #app.run(debug=True, host="0.0.0.0", port=3000)
