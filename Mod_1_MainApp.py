92e5c0db135dae88208026474b9a72c8281ec09a
diego-faria-br <diego-faria-br@users.noreply.github.com>
2020-11-15 16:13:25 +0100

    Ok

--- Mod_1_MainApp.py
+++ Mod_1_MainApp.py
@@ -4,12 +4,15 @@ app = Flask(__name__)
 from Mod_2_API import gather_twits
 
 
+
 @app.route("/")
 def homepage():
     """View function for Home Page."""
-    twits = gather_twits(n=1, df=False)
+    twits = gather_twits(n=3, df=True)
     return render_template("home.html", twits = twits)
 
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=3000)
+    
+    
