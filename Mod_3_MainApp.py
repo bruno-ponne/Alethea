1176a3d3bd07a23e655c94c93247a78be7385b00
Bruno Ponne <b.gasparotto-ponne@mpp.hertie-school.org>
2020-12-01 19:48:49 +0100

    01_12_update

--- Mod_3_MainApp.py
+++ Mod_3_MainApp.py
@@ -6,6 +6,7 @@ from Mod_2_DataAnalysis import draw_graph
 from Mod_2_DataAnalysis import delete_plots
 from Mod_2_DataAnalysis import compare_tweets
 from Mod_2_DataAnalysis import word_cloud
+from Mod_2_DataAnalysis import sentiment_graphs
 
 
 @app.route("/")
@@ -14,13 +15,19 @@ def homepage():
     delete_plots()
     name_plot = draw_graph(10)
     name_word_cloud = word_cloud()
+    sentiment_analysis1 = sentiment_graphs("Politifact")
+    sentiment_analysis2 = sentiment_graphs("factcheckdotorg")
+    sentiment_analysis3 = sentiment_graphs("snopes")
+        
     tweets = gather_tweets(n=10, df=False)
     return render_template("home.html", 
                            tweets = tweets, 
                            name_plot = name_plot,
-                           name_word_cloud = name_word_cloud)
+                           name_word_cloud = name_word_cloud, 
+                           sentiment_analysis1 = sentiment_analysis1,
+                           sentiment_analysis2 = sentiment_analysis2,
+                           sentiment_analysis3 = sentiment_analysis3)
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=3000)
-
+    app.run()
